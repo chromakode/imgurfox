a4046f37ad96b9f2fd293cfd2aa1ab0b803dcbde
Chromakode <chromakode@gmail.com>
2009-09-15 12:25:48 -0700

    Fix indentation to be consistent.

--- content/imgurfox.js
+++ content/imgurfox.js
@@ -5,7 +5,11 @@ var ImgurFoxWindow = (function() {
   let extensionReg = /\.(jpg|jpeg|gif|png|apng|tiff|bmp|pdf|xcf)(\?.*)?$/i;
   
   function uploadURL(imgSrc) {
-    return "http://imgur.com/api/upload/?url=" + imgSrc;
+    return "http://imgur.com/api/upload?url=" + imgSrc;
+  }
+  
+  function editURL(imgSrc) {
+    return "http://imgur.com/api/edit?url=" + imgSrc;
   }
 
   var ImgurFoxWindow = {
@@ -15,37 +19,37 @@ var ImgurFoxWindow = (function() {
     },
     
     onLoad: function() {
-        let contextMenu = document.getElementById("contentAreaContextMenu");
-        let uploadMenuItem = document.getElementById("context-imgur");
-        contextMenu.addEventListener("popupshowing", function(event) {
-            let imageURI = null;
-            if (gContextMenu.onImage) {
-                // Right clicked on an image
-                imageURI = gContextMenu.target.currentURI;
-            } else if (gContextMenu.onLink && extensionReg.test(gContextMenu.linkURL)) {
-                // Right clicked on a link to an image
-                imageURI = gContextMenu.linkURI;
-            }
-            
-            if (imageURI) {
-                // TODO: Can imgur take https or ftp URL schemes?
-                schemeOk = imageURI.scheme == "http";
-                uploadMenuItem.hidden = !schemeOk;
-            } else {
-                uploadMenuItem.hidden = true;
-            }
-        }, false)
+      let contextMenu = document.getElementById("contentAreaContextMenu");
+      let uploadMenuItem = document.getElementById("context-imgur");
+      contextMenu.addEventListener("popupshowing", function(event) {
+        let imageURI = null;
+        if (gContextMenu.onImage) {
+          // Right clicked on an image
+          imageURI = gContextMenu.target.currentURI;
+        } else if (gContextMenu.onLink && extensionReg.test(gContextMenu.linkURL)) {
+          // Right clicked on a link to an image
+          imageURI = gContextMenu.linkURI;
+        }
+        
+        if (imageURI) {
+          // TODO: Can imgur take https or ftp URL schemes?
+          schemeOk = imageURI.scheme == "http";
+          uploadMenuItem.hidden = !schemeOk;
+        } else {
+          uploadMenuItem.hidden = true;
+        }
+      }, false)
     },
     
     onUnload: function() {
-        // :(
+      // :(
     },
     
     contextUpload: function(event) {
-        if (gContextMenu.onImage)
-          openUILinkIn(uploadURL(gContextMenu.imageURL), "tab");
-        else if (gContextMenu.onLink)
-          openUILinkIn(uploadURL(gContextMenu.linkURL), "tab");
+      if (gContextMenu.onImage)
+        openUILinkIn(uploadURL(gContextMenu.imageURL), "tab");
+      else if (gContextMenu.onLink)
+        openUILinkIn(uploadURL(gContextMenu.linkURL), "tab");
     },
 
   }
