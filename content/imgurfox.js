79b9abab38fa999a8fe5c5102702229bd21e8757
Chromakode <chromakode@gmail.com>
2009-09-15 11:19:46 -0700

    Refactor Kory's image link code.

--- content/imgurfox.js
+++ content/imgurfox.js
@@ -1,10 +1,11 @@
 var ImgurFoxWindow = (function() {
   let modules = {};
   let importModule = function(name) Components.utils.import(name, modules);
-  let extensionReg = "gif jpg jpeg png";
+  
+  let extensionReg = /\.(jpg|jpeg|gif|png|apng|tiff|bmp|pdf|xcf)(\?.*)?$/i;
   
   function uploadURL(imgSrc) {
-    return "http://imgur.com/api/upload/?url=" + imgSrc
+    return "http://imgur.com/api/upload/?url=" + imgSrc;
   }
 
   var ImgurFoxWindow = {
@@ -17,16 +18,20 @@ var ImgurFoxWindow = (function() {
         let contextMenu = document.getElementById("contentAreaContextMenu");
         let uploadMenuItem = document.getElementById("context-imgur");
         contextMenu.addEventListener("popupshowing", function(event) {
+            let imageURI = null;
             if (gContextMenu.onImage) {
+                // Right clicked on an image
+                imageURI = gContextMenu.target.currentURI;
+            } else if (gContextMenu.onLink && extensionReg.test(gContextMenu.linkURL)) {
+                // Right clicked on a link to an image
+                imageURI = gContextMenu.linkURI;
+            }
+            
+            if (imageURI) {
                 // TODO: Can imgur take https or ftp URL schemes?
-                let schemeOk = gContextMenu.target.currentURI.scheme == "http"
+                schemeOk = imageURI.scheme == "http";
                 uploadMenuItem.hidden = !schemeOk;
-            } else if (gContextMenu.onLink) {
-                let extension = gContextMenu.linkURL.split(".").pop();
-                if (extension != null)
-                  uploadMenuItem.hidden = !(extensionReg.match(extension)==extension);
-            }            
-            else {
+            } else {
                 uploadMenuItem.hidden = true;
             }
         }, false)
@@ -37,7 +42,6 @@ var ImgurFoxWindow = (function() {
     },
     
     contextUpload: function(event) {
-
         if (gContextMenu.onImage)
           openUILinkIn(uploadURL(gContextMenu.imageURL), "tab");
         else if (gContextMenu.onLink)
