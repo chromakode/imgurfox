aa28d8782660eeb5e3815586319b5b2616491ae2
chromakode <chromakode@gmail.com>
2009-10-12 17:12:33 -0700

    Basic screenshot capability (full pages; no error catching)

--- content/imgurfox.js
+++ content/imgurfox.js
@@ -3,12 +3,16 @@ var ImgurFoxWindow = (function() {
   let stringBundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
                                .getService(Components.interfaces.nsIStringBundleService)
                                .createBundle("chrome://imgurfox/locale/imgurfox.properties");
+  let nativeJSON = Components.classes["@mozilla.org/dom/json;1"]
+                             .createInstance(Components.interfaces.nsIJSON);
   
   let imageFileReg = /\.(jpg|jpeg|gif|png|apng|tiff|bmp|pdf|xcf)(\?.*)?$/i;
   
-  CONTEXT_CHOICE = 0;
-  CONTEXT_UPLOAD = 1;
-  CONTEXT_EDIT   = 2;
+  let IMGUR_API_KEY = "24bf6070f45ed716e8cf9324baebddbd";
+  
+  let CONTEXT_CHOICE = 0;
+  let CONTEXT_UPLOAD = 1;
+  let CONTEXT_EDIT   = 2;
   
   function uploadURL(imgSrc) {
     return "http://imgur.com/api/upload?url=" + imgSrc;
@@ -17,6 +21,8 @@ var ImgurFoxWindow = (function() {
   function editURL(imgSrc) {
     return "http://imgur.com/api/upload?edit&url=" + imgSrc;
   }
+  
+  let uploadPOSTURL = "http://imgur.com/api/upload.json";
 
   var ImgurFoxWindow = {
     init: function() {
@@ -81,6 +87,89 @@ var ImgurFoxWindow = (function() {
       }
       let actionURL = edit ? editURL : uploadURL;
       openUILinkIn(actionURL(ImgurFoxWindow.contextImageURI.spec), "tab");
+    },
+    
+    grabScreenshot: function() {
+      let canvas = document.getElementById("imgurfox-canvas");
+      let win = gBrowser.contentWindow;
+      let x = win.scrollX;
+      let y = win.scrollY;
+      let w = win.document.width;
+      let h = win.innerHeight; 
+      
+      canvas.width = w;
+      canvas.height = h;
+      
+      let ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, w, h);
+      ctx.drawWindow(win, x, y, w, h, "rgb(255,255,255)");
+      return canvas.toDataURL();
+    },
+    
+    screenshotUpload: function(event) {
+      // Many thanks to Brad Greenlee for his concise example:
+      // http://blog.footle.org/2007/07/31/binary-multipart-posts-in-javascript/
+      
+      let dataURL = this.grabScreenshot();
+      let base64data = dataURL.replace(/^([^,])*,/, "");
+      let binaryData = atob(base64data);
+
+      let timestamp = Date.now();
+      let separator = "----------multipart-boundary-" + timestamp;
+
+      let prefixStringInputStream = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream);
+      let formData =
+        "--" + separator + "\r\n" +
+	    "Content-Disposition: form-data; name=\"image\"; filename=\"image_" + timestamp + ".png" + "\"\r\n" +
+	    "Content-Type: image/png\r\n\r\n";
+      prefixStringInputStream.setData(formData, formData.length);
+
+      // write the image data via a binary output stream, to a storage stream
+      var binaryOutputStream = Components.classes["@mozilla.org/binaryoutputstream;1"].createInstance(Components.interfaces.nsIBinaryOutputStream);
+      var storageStream = Components.classes["@mozilla.org/storagestream;1"].createInstance(Components.interfaces.nsIStorageStream);
+      storageStream.init(4096, binaryData.length, null);
+      binaryOutputStream.setOutputStream(storageStream.getOutputStream(0));
+      binaryOutputStream.writeBytes(binaryData, binaryData.length);
+      binaryOutputStream.close();
+
+      // write out the rest of the form to another string input stream
+      var suffixStringInputStream = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream);
+      formData =
+        "\r\n--" + separator + "\r\n" +
+        "Content-Disposition: form-data; name=\"key\"\r\n\r\n" + IMGUR_API_KEY + "\r\n" +
+        "--" + separator + "--\r\n";
+      suffixStringInputStream.setData(formData, formData.length);
+
+      // multiplex the streams together
+      var multiStream = Components.classes["@mozilla.org/io/multiplex-input-stream;1"].createInstance(Components.interfaces.nsIMultiplexInputStream);
+      multiStream.appendStream(prefixStringInputStream);
+      multiStream.appendStream(storageStream.newInputStream(0));
+      multiStream.appendStream(suffixStringInputStream);
+    
+      let req = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+      req.open("POST", uploadPOSTURL, true);
+      req.setRequestHeader("Content-type", "multipart/form-data; boundary=" + separator);
+      req.setRequestHeader("Content-length", multiStream.available());
+      req.onreadystatechange = function (e) {
+        if (req.readyState == 4) {
+          if(req.status == 200) {
+            data = nativeJSON.decode(req.responseText)
+            if (data["rsp"]["stat"] == "ok") {
+              openUILinkIn(data["rsp"]["image"]["imgur_page"], "tab");
+            } else {
+              dump("Imgur error: " + data["rsp"]["error_code"]);
+              // FIXME
+            }
+          } else {
+            // FIXME
+          }
+        }
+      };
+      req.onprogress = function onProgress(e) {  
+        let percentComplete = (e.position / e.totalSize)*100;
+        dump(percentComplete);
+      }  
+      req.send(multiStream);
     },
 
   }
